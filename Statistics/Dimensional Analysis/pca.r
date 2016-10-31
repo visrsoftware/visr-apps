73f53948f398bb5d6a3ede34f91f61d55cf6555e
Hamid <hyounesy@gmail.com>
2016-10-31 14:09:32 -0700

    fixed broken PCA. added option to export the PCA output as a new table. also to output eigen vectors.

--- Statistics/Dimensional Analysis/pca.r
+++ Statistics/Dimensional Analysis/pca.r
@@ -1,29 +1,34 @@
+visr.applyParameters()
 
-###applyParameters
+input_scores <- TRUE # should this be moved to the parameters?
 
-
-output_princomp <- princomp(x = input_table[,input_columns], cor = input_cor, scores = input_scores)
+output_princomp <- princomp(x = visr.input[,input_columns], cor = input_cor, scores = input_scores)
 # summary(output_princomp) # print variance accounted for 
 # loadings(output_princomp) # pc loadings 
 
-{{
-  if (input_choiceofplot == "biplot") {
+if (input_choiceofplot == "biplot") {
     biplot(output_princomp) # screeplot 
-  }else{
-    plot(output_princomp,type="lines")
-  }
-}}
+} else {
+    plot(output_princomp, type="lines")
+}
 
-if(input_scores == TRUE) {ouput_scores <- output_princomp$scores} # the principal components
+#if(input_scores == TRUE) {
+output_scores <- output_princomp$scores
+if (length(concat_columns) > 0) {
+	output_scores <- cbind(visr.input[,concat_columns], output_scores)
+}
 
-print(capture.output(cat("\nComponent loadings:\n")),collapse='\n')
-print(capture.output(print(unclass(loadings(output_princomp)))),collapse='\n')
+output_ev <- as.matrix(output_princomp$loadings[])
+#} # the principal components
+
+#print(capture.output(cat("\nComponent loadings:\n")),collapse='\n')
+#print(capture.output(print(unclass(loadings(output_princomp)))),collapse='\n')
 #print(unclass(loadings(output_princomp)))
 
-print(capture.output(cat("\nComponent variances:\n")),collapse='\n')
-print(capture.output(print(output_princomp$sd^2)),collapse='\n')
+#print(capture.output(cat("\nComponent variances:\n")),collapse='\n')
+#print(capture.output(print(output_princomp$sd^2)),collapse='\n')
 
 
-print(capture.output(cat("\n")),collapse='\n')
-print(capture.output(print(summary(output_princomp))),collapse='\n')
+#print(capture.output(cat("\n")),collapse='\n')
+#print(capture.output(print(summary(output_princomp))),collapse='\n')
 
