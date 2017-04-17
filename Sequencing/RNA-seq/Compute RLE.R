e17269190b631141e67522e43eef46722b80112b
Hamid <hyounesy@gmail.com>
2017-04-17 16:44:36 -0700

    updated app: RLE and k-means

--- Sequencing/RNA-seq/Compute RLE.R
+++ Sequencing/RNA-seq/Compute RLE.R
@@ -1,28 +1,32 @@
-#rm(list=ls())
-#input_table <- read.table("/Users/hyounesy/Research/Data/VisRseq/error/041616-Ref-seq.xls",sep="\t",header=TRUE)
-#input_columns<-c("WT1count","WT2count","WT3count")
-#input_columns<-c("WT1RPKM","WT2RPKM","WT3RPKM")
-
 source("visrutils.R")
 
+visr.app.start("Relative Log Expression (RLE)", info = "The log-ratio of the read count of each gene to the median read count across samples")
+visr.param("columns", type = "multi-column-numerical", label = "Columns to calculate RLE for")
+visr.param("addvalue", type = "double", label = "Add number", default = 0.0, info="The number to add to all counts (necessary if there is 0 in data)")
+visr.param("output_RLE", type = "output-multi-column", label = "Output Columns Prefix", info ="RLE values of columns")
+visr.app.end(printjson=TRUE, writefile=T)
 visr.applyParameters()
 
-{{
-  generateRLE <- function (object) {
-    nARR = dim(object)[2]
-    nGEN = dim(object)[1]
-    y = apply(object, 1, median)
-    y_median<<-y
-    rle = matrix(nrow = nGEN, ncol = nARR)
-    for (i in 1:nARR) {
-      x = object[, i]
-      rle[, i] = log(x/y)
-    }
-    colnames(rle) = colnames(object)
-    return(rle)
-  }
-}}
+visr.biocLite("edgeR")
 
-output_RLE<-generateRLE(subset(input_table, select = input_columns))
+generateRLE <- function (object) {
+  nARR = dim(object)[2]
+  nGEN = dim(object)[1]
+  y = apply(object, 1, median)
+  y_median<<-y
+  rle = matrix(nrow = nGEN, ncol = nARR)
+  for (i in 1:nARR) {
+    x = object[, i]
+    rle[, i] = log(x/y, 10)
+  }
+  colnames(rle) = colnames(object)
+  return(rle)
+}
+
+X <- subset(visr.input, select = visr.param.columns)
+X <- X + visr.param.addvalue
+f <- calcNormFactors(X) # Calculate normalization factors to scale the raw library sizes.
+
+visr.param.output_RLE<-generateRLE(X*f)
 par(mfrow=c(1,1))
 boxplot(x = output_RLE, xlab = "Output", ylab = "RLE", main = "RLE output", outline = FALSE)
--- Statistics/Clustering/K-Means Clustering.R
+++ Statistics/Clustering/K-Means Clustering.R
@@ -1,65 +1,57 @@
+source("visrutils.R")
+visr.library("ggplot2")
 
-usePackage<-function (pkg) {if (!require(pkg, character.only = TRUE)) {install.packages(pkg, repos = "http://cran.us.r-project.org");require(pkg, character.only = TRUE)}}
-
-usePackage("NbClust")
-
+# start parameter definition
+visr.app.start("K-means Clustering", debugdata = iris)
+visr.category("clustering parameters")
+visr.param("columns", type = "multi-column-numerical")
+visr.param("k", label = "Number of Clusters (K)", default = 3L, min = 1L)
+visr.param("algorithm", label = "Algorithm", items = c("Hartigan-Wong", "Lloyd", "Forgy", "MacQueen"))
+visr.param("sortids", label="Sort cluster IDs by size",  default = TRUE)
+visr.category("output")
+visr.param("plottype", label = "Summary Plot", items = c("barplot", "splom"), item.labels = c("Barplot of Cluster Sizes", "Scatterplot Matrix"))
+visr.param("output.clusterid", label = "Column name to output cluster IDs", type = "output-column", default = "clusterid")
+visr.param("output.clustermeans", label = "Table name to output cluster means", type = "output-table", default = "")
+visr.app.end(writefile = TRUE, printjson=TRUE)
 
 visr.applyParameters()
 
-#1 standardize data
+#run kmeans
+cluster_data<-subset(visr.input, select = visr.param.columns)
+kmeans_result <- kmeans(cluster_data, visr.param.k, algorithm = visr.param.algorithm)
 
-data <- input_table[,input_columns]
-if (input_scale){ data = scale(data)}
+#prepare results
+clusterid <- kmeans_result$cluster
+clustercenters <- data.frame(kmeans_result$centers, row.names = NULL)
+clustercenters[,"clusterID"] <- as.factor(rownames(clustercenters))
+rownames(clustercenters) <- NULL
 
-#2 determine number of clusters
-
-#3 K-means cluster analysis
-
-{{
-  wssplot <- function(data, nc, seed=1234){
-  wss <- (nrow(data)-1)*sum(apply(data,2,var))
-  for (i in 2:nc){
-    set.seed(seed)
-    wss[i] <- sum(kmeans(data, centers=i)$withinss)
+# sort results
+if (visr.param.sortids) {
+  ids_sorted <- dimnames(sort(table(clusterid), decreasing = T))[[1]]
+  sorted_clusterid <- clusterid
+  sorted_clustercenters <- clustercenters
+  for (c in 1:length(ids_sorted)) {
+    sorted_clusterid[which(clusterid == ids_sorted[c])] <- c
+    sorted_clustercenters[ids_sorted[c], "clusterID"] <- c
   }
-  plot(1:nc, wss, type="b", xlab="Number of Clusters",
-       ylab="Within groups sum of squares")
+  clusterid <- sorted_clusterid
+  clustercenters <- sorted_clustercenters
 }
-}}
 
-oldpar <- par()  # make a copy of current graphical settings
-set.seed(1234) # determining the best number of clusters
-nc2 <- NbClust(data, min.nc = 2, max.nc = input_nc, method="kmeans")
-table(nc2$Best.n[1,]) # restore original settings
-par(oldpar)
+visr.param.output.clusterid <- as.factor(clusterid)
+visr.param.output.clustermeans <- clustercenters
 
-
-
-{{
-  output_kmeans <- kmeans(data, 
-                        centers = input_centers,
-                        iter.max = input_itermax,
-                        nstart = input_nstart,
-                        algorithm = input_algorithm)
-}}
-
-output_clusterid <- output_kmeans$cluster
-print(capture.output(print(output_kmeans)),collapse='\n')
-print(paste("total sum of squares", eval(output_kmeans$totss),"        between sum of squares", eval(output_kmeans$betweenss),"        total within sum of squares", eval(output_kmeans$tot.withinss)))
-
-
-{{
-  if (input_choiceofplot == "Plot the within groups sums of squares vs. the number of clusters extracted"){
-    wssplot(data, nc = input_nc) # A bend in the graph can suggest the appropriate number of clusters.
-  }else{
-    if (input_choiceofplot == "Recommended number of clusters using 26 criteria provided by the NbClust package"){
-      barplot(table(nc2$Best.n[1,]), 
-              xlab="Numer of Clusters", ylab="Number of Criteria",
-              main="Number of Clusters Chosen by 26 Criteria")
-    }else{
-      if (input_choiceofplot == "K-means clustering"){
-        plot(data, main="K-means clustering", col=as.integer(output_clusterid))
-      }
-    }
-  }
-}}
+if (visr.param.plottype == "barplot") {
+  #barplot(table(factor(clusterid)))
+  c <- ggplot(visr.input, aes(factor(visr.param.output.clusterid))) +
+       theme_bw() +
+       geom_bar(fill = "grey40") +
+       labs(title = paste("k-means clusters for k =", visr.param.k), x = "cluster id", y = "count")
+  print(c)
+} else {
+  # warning: scatterplot matrices for large data will take forever to render
+  plot(cluster_data,
+       main = paste("Scatter plot matrix for k =", visr.param.k),
+       col = as.integer(visr.param.output.clusterid))
+}
