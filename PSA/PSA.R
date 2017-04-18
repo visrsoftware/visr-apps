3a6e95a15e89a00fc2147f1a6161b60a2160f16e
Hamid <hyounesy@gmail.com>
2017-04-18 16:42:27 -0700

    RLE: output size normalized columns

--- PSA/PSA.R
+++ PSA/PSA.R
@@ -5,51 +5,93 @@ source("frequency.R")
 visr.applyParameters()
 
 if (FALSE) {
-# Hamid's DEBUG:
-#visr.param.directory <- "/Users/hyounesy/Research/Data/VisRseq/Runs/DESeq_EdgeR_LinSca"
+  ### FOR TESTING ONLY - apply variables manually
 
-### FOR TESTING ONLY - apply variables manually
-# source("D:/Joseph/visrseq/visr/srcR/frequency.R")
-# # source("C:/Users/Joseph/Documents/visrseq/visr/srcR/frequency.R")
-# # source("B:/Diverses/Dropbox/Uni/BA/VisRSeq/visr/srcRfrequency.R")
-# visr.param.directory <-"D:/Joseph/visrseq/output/k-means_2016-3-29(16.57)[20]_iris"
-# # visr.param.directory <- "B:/Diverses/Dropbox/Uni/BA/VisRSeq/output/LinSca_Lin-Pos-Neg_edgeR_115-9-22(6.43)[550]"
-# # visr.param.directory <- "C:/Users/Joseph/Documents/visrseq/output/LinSca_Lin-Pos-Neg_edgeR_115-9-22(6.43)[550]"
-# # visr.param.directory <- "C:/Users/Joseph/Documents/visrseq/output/test-method"
-# # visr.param.directory <- "C:/Users/Joseph/Documents/visrseq/output/k-means_2016-1-26(18.14)[6]_iris-sub"
-# visr.param.mdsColumnIndex <- 1
-# visr.param.summaryUpDown <- "sum_"
-# visr.param.recalc <- T
-# visr.param.mdsMethod <- "manhattan"
-# visr.output.summaryMDS <- "mds_"
-###
+  # Joseph's Debug:
+  # source("D:/Joseph/visrseq/visr/srcR/frequency.R")
+  # # source("C:/Users/Joseph/Documents/visrseq/visr/srcR/frequency.R")
+  # # source("B:/Diverses/Dropbox/Uni/BA/VisRSeq/visr/srcRfrequency.R")
+  # visr.param.directory <-"D:/Joseph/visrseq/output/k-means_2016-3-29(16.57)[20]_iris"
+  # # visr.param.directory <- "B:/Diverses/Dropbox/Uni/BA/VisRSeq/output/LinSca_Lin-Pos-Neg_edgeR_115-9-22(6.43)[550]"
+  # # visr.param.directory <- "C:/Users/Joseph/Documents/visrseq/output/LinSca_Lin-Pos-Neg_edgeR_115-9-22(6.43)[550]"
+  # # visr.param.directory <- "C:/Users/Joseph/Documents/visrseq/output/test-method"
+  # # visr.param.directory <- "C:/Users/Joseph/Documents/visrseq/output/k-means_2016-1-26(18.14)[6]_iris-sub"
+  # visr.param.mdsColumnIndex <- 1
+  # visr.param.summaryUpDown <- "sum_"
+  # visr.param.recalc <- T
+  # visr.param.mdsMethod <- "manhattan"
+  # visr.output.summaryMDS <- "mds_"
+  ###
 
-.libPaths(c("/Users/hyounesy/VisRseq/RLibs", .libPaths()))
-source("visrutils.R")
-source("frequency.R")
-#visr.param.directory<-"/Users/hyounesy/Research/Data/edgeR_2016-3-30(18.6)[500]_mydata_5spc"
-visr.param.directory<-"/Users/hyounesy/Research/Data/PSA/edgeR_2016-4-14(7.51)[1000]_Jafar_SNwt_ESC_vs_SNcDKO_ESC"
-visr.param.mdsColumnIndex<-1
-visr.param.summaryUpDown<-"sum_"
-visr.param.recalc<-TRUE
-visr.param.mdsMethod<-"manhattan"
-visr.output.summaryMDS<-"mds_"
+  # Hamid's DEBUG:
+  #visr.param.directory1 <- "/Users/hyounesy/Research/Data/VisRseq/Runs/DESeq_EdgeR_LinSca"
+  .libPaths(c("/Users/hyounesy/VisRseq/RLibs", .libPaths()))
+  source("visrutils.R")
+  source("frequency.R")
+  visr.param.directory1<-"/Users/hyounesy/Research/Data/edgeR_2016-3-30(18.6)[500]_mydata_5spc"
+  #visr.param.directory1<-"/Users/hyounesy/Research/Data/PSA/edgeR_2016-4-14(7.51)[1000]_Jafar_SNwt_ESC_vs_SNcDKO_ESC"
+  #visr.param.directory1<-"/Users/hyounesy/Research/svn/Projects/GeneCalc/output/edgeR[4000]_GSE49712_SEQC_Count"
+
+  visr.param.mdsColumnIndex<-1
+  visr.param.summaryUpDown<-"sum_"
+  visr.param.recalc<-TRUE
+  visr.param.mdsMethod<-"manhattan"
+  visr.output.summaryMDS<-"mds_"
 }
 
 
+path <- visr.param.directory1
+
+visr.print("loading the index files")
+pathInput               <- paste(path, "/input.txt", sep = "")
+pathIndex               <- paste(path, "/index.txt", sep="")
+pathParamInfo           <- paste(path, "/paramInfo.txt", sep="")
+pathAllRunsMatrixRData  <- paste(path, "/allRunsMatrix.RData", sep="")
+pathAllRunsMatrixTxt    <- paste(path, "/allRunsMatrix.txt", sep="")
+pathDistanceMatrix      <- paste(path, "/distanceMatrix.txt" ,sep="")
+pathDistanceMatrixRData <- paste(path, "/distanceMatrix.RData" ,sep="")
+pathImpact              <- paste(path, "/impact.txt", sep="")
+pathFrequency           <- paste(path, "/frequency.txt",sep = "")
+pathStableGenes         <- paste(path, "/stableGenes.txt",sep = "")
+
 allRunsMatrix <- NULL
+distanceMatrix <- NULL
+
 loadAllRunsMatrixIfNull<-function() {
   if (is.null(allRunsMatrix)) {
-    visr.print("loading the allRunsMatrix.txt")
-    allRunsMatrix <<- read.table(paste(visr.param.directory1, "/allRunsMatrix.txt", sep=""), header=FALSE, sep="\t", check.names = F)
+    if (file.exists(pathAllRunsMatrixRData)) {
+      # first try to load the binary version
+      visr.print(paste("loading", pathAllRunsMatrixRData))
+      load(file = pathAllRunsMatrixRData, .GlobalEnv)
+    }
+  }
+
+  if (is.null(allRunsMatrix)) {
+    # could not load the binary, try the txt one
+    visr.print(paste("loading", pathAllRunsMatrixTxt))
+    allRunsMatrix <<- read.table(pathAllRunsMatrixTxt, header=FALSE, sep="\t", check.names = F)
+    # write the binary
+    visr.print(paste("saving", pathAllRunsMatrixRData))
+    save(allRunsMatrix, file = pathAllRunsMatrixRData)
   }
 }
 
-distanceMatrix <- NULL
-loadDistanceMatrixIfNull<-function(){
+loadDistanceMatrixIfNull<-function() {
   if (is.null(distanceMatrix)) {
-    visr.print("loading the distanceMatrix.txt")
-    distanceMatrix <<- read.table(paste(visr.param.directory1, "/distanceMatrix.txt" ,sep=""), sep="\t", check.names = F)
+    if (file.exists(pathDistanceMatrixRData)) {
+      # first try to load the binary version
+      visr.print(paste("loading", pathDistanceMatrixRData))
+      load(file = pathDistanceMatrixRData, .GlobalEnv)
+    }
+  }
+
+  if (is.null(distanceMatrix)) {
+    # could not load the binary, try the txt one
+    visr.print(paste("loading", pathDistanceMatrix))
+    distanceMatrix <<- read.table(pathDistanceMatrix, sep="\t", check.names = F)
+    # write the binary
+    visr.print(paste("saving", pathDistanceMatrixRData))
+    save(distanceMatrix, file = pathDistanceMatrixRData)
   }
 }
 
@@ -65,13 +107,8 @@ storeMinDistColumns <- FALSE
 ###############################################################################
 # Setting paths and loading crucial index files
 ###############################################################################
-path <- visr.param.directory1
 
 visr.print("loading the index files")
-pathIndex <- paste(path, "/index.txt", sep="")
-pathParamInfo <- paste(path, "/paramInfo.txt", sep="")
-pathInput = paste(path, "/input.txt", sep = "")
-
 inputTable <- read.table(pathInput, header=TRUE,sep="\t", check.names = F)
 paramInfo  <- read.table(pathParamInfo, header=TRUE,sep = "\t", stringsAsFactors = FALSE, check.names = F)
 indexTable <- read.table(pathIndex, header=TRUE, sep = "\t", check.names = F)
@@ -96,9 +133,8 @@ colnames(upDown) <- c(upColName, downColName)
 
 # !(upColName %in% colnames(indexTable)) ||    # check if index table has the number of up-regulated genes for each run
 # 	!(downColName %in% colnames(indexTable)) ||  # check if index table has the number of down-regulated genes for each run
-if (
-	  visr.param.recalc || # force recalculating
-    !file.exists(paste(path,"/allRunsMatrix.txt",sep="")) )
+if (visr.param.recalc || # force recalculating
+    (!file.exists(pathAllRunsMatrixRData) && !file.exists(pathAllRunsMatrixTxt)))
 {
   visr.print("Recreate the matrix containing the up/down regulation state for all runs")
   # Recreate the matrix containing the up/down regulation state for all runs
@@ -117,14 +153,19 @@ if (
       allRunsMatrix[i,] <- tt[, visr.param.mdsColumnIndex]
     }
   }
-  write.table(allRunsMatrix, file=paste(path,"/allRunsMatrix.txt",sep=""),col.names = F, row.names = F, sep = "\t")
+  #write.table(allRunsMatrix, file=paste(path,"/allRunsMatrix.txt",sep=""),col.names = F, row.names = F, sep = "\t")
+  save(allRunsMatrix, file = pathAllRunsMatrixRData)
+
   indexTable[[upColName]] <- upDown[,1]
   indexTable[[downColName]] <- upDown[,2]
+
+  # writing the table, in case of a crash, etc...
+  write.table(indexTable, file=pathIndex, row.names = FALSE, col.names = TRUE, sep = "\t")
 
   visr.print("[DONE] Recreate the matrix containing the up/down regulation state for all runs")
 
 } else {
-	#optimization: delay loading this until neede:
+	#optimization: delay loading this until needed:
   #allRunsMatrix <- loadAllRunsMatrix(path)
 
 # 	upDown[,1] <- indexTable[[upColName]]
@@ -140,12 +181,12 @@ if(! downColName %in% paramInfo$label) {
 }
 
 
-#plot(upDown)
+# plot(upDown)
 
 ###############################################################################
 # Calculate Distance matrix (distance between runs)
 ###############################################################################
-if (!file.exists(paste(path,"/distanceMatrix.txt",sep="")) || # whether the distance matrix was already computed and saved
+if ((!file.exists(pathDistanceMatrixRData) && !file.exists(pathDistanceMatrix)) || # whether the distance matrix was already computed and saved
     visr.param.recalc) # forcing the recalculation
 {
   loadAllRunsMatrixIfNull()
@@ -164,13 +205,15 @@ if (!file.exists(paste(path,"/distanceMatrix.txt",sep="")) || # whether the dist
 	} else {
 	  distanceMatrix <- as.matrix(dist(allRunsMatrix, method = visr.param.mdsMethod)) #  distances between the rows
 		# save distanceMatrix to a file
-		write.table(distanceMatrix, file=paste(path,"/distanceMatrix.txt",sep=""),col.names = F, row.names = F, sep = "\t")
+		# write.table(distanceMatrix, file=pathDistanceMatrix, col.names = F, row.names = F, sep = "\t")
+		visr.print(paste("saving", pathDistanceMatrixRData))
+		save(distanceMatrix, file = pathDistanceMatrixRData)
 	}
   visr.print("[DONE] recompute the distance matrix.")
 } else {
   # load the existing distance matrix from file.
   # delay loading this until needed (performance optimization):
-  #distanceMatrix <- read.table(paste(path, "/distanceMatrix.txt" ,sep=""), sep="\t", check.names = F)
+  #distanceMatrix <- read.table(pathDistanceMatrix, sep="\t", check.names = F)
 }
 
 ###############################################################################
@@ -201,8 +244,9 @@ if ( !all(mdsColNames %in% colnames(indexTable)) || visr.param.recalc == TRUE) {
 ###############################################################################
 # Calculate Parameters' significance/impact score
 ###############################################################################
+
 if(visr.param.recalc ||
-   !file.exists(paste(path, "/impact.txt", sep=""))) # whether the impact file exists already
+   !file.exists(pathImpact)) # whether the impact file exists already
 {
   loadAllRunsMatrixIfNull()
   loadDistanceMatrixIfNull()
@@ -321,13 +365,13 @@ if(visr.param.recalc ||
 
 	# Order and save the sigFrame
 	sigFrame <- sigFrame[with(sigFrame, order(-sig)), ]
-	write.table(as.matrix(sigFrame), file=paste(path,"/impact.txt",sep=""), col.names = NA, sep = "\t")
+	write.table(as.matrix(sigFrame), file = pathImpact, col.names = NA, sep = "\t")
 }
 
 # visr.message("after impact")
 
 # Generate the frequency / stability of the assigned clusters to the genes
-if(!file.exists(paste(path,"/frequency.txt",sep = "")) || !file.exists(paste(path,"/stableGenes.txt",sep = "")) || visr.param.recalc == TRUE) {
+if(!file.exists(pathFrequency) || !file.exists(pathStableGenes) || visr.param.recalc == TRUE) {
   visr.print("Generate the frequency / stability of the assigned clusters to the genes")
 
   loadAllRunsMatrixIfNull()
@@ -337,21 +381,21 @@ if(!file.exists(paste(path,"/frequency.txt",sep = "")) || !file.exists(paste(pat
 	f$name <- inputTable$name # Add the names of the genes to the table for easier identification
 	f <- f[with(f, order(-freq)), ] # Order them descending for frequency
 	top <- f[(f$sym != "0") & (f$freq > 0.95),] # Assable the top (most stable) genes which are not unregulate
-	write.table(f, file=paste(path,"/frequency.txt",sep = ""), row.names = TRUE, col.names = NA, sep = "\t")
-	write.table(top, file=paste(path,"/stableGenes.txt",sep = ""), row.names = TRUE, col.names = NA, sep = "\t")
+	write.table(f, file=pathFrequency, row.names = TRUE, col.names = NA, sep = "\t")
+	write.table(top, file=pathStableGenes, row.names = TRUE, col.names = NA, sep = "\t")
 
   visr.print("[DONE] Generate the frequency / stability of the assigned clusters to the genes")
 } else {
-	f <- read.table(paste(path, "/frequency.txt", sep = ""), header=TRUE,sep="\t", check.names = F)
-	top <- read.table(paste(path, "/stableGenes.txt", sep = ""), header=TRUE,sep="\t", check.names = F)
+	f <- read.table(pathFrequency, header=TRUE,sep="\t", check.names = F)
+	top <- read.table(pathStableGenes, header=TRUE,sep="\t", check.names = F)
 }
 
 # Save the possibly changed files
-visr.print("Save the possibly changed files")
+visr.print("Save the (possibly) changed files")
 
 # don't rewrite inputTable as the modes are now calculated in realtime in code
 #write.table(inputTable, file=pathInput,     row.names = FALSE, col.names = TRUE, sep = "\t")
 
 write.table(indexTable, file=pathIndex,     row.names = FALSE, col.names = TRUE, sep = "\t")
 write.table(paramInfo,  file=pathParamInfo, row.names = FALSE, col.names = TRUE, sep = "\t")
-visr.print("[DONE] Save the possibly changed files")
+visr.print("[DONE] Save the (possibly) changed files")
--- Sequencing/RNA-seq/Compute RLE.R
+++ Sequencing/RNA-seq/Compute RLE.R
@@ -4,9 +4,11 @@ visr.app.start("Relative Log Expression (RLE)", info = "The log-ratio of the rea
 visr.param("columns", type = "multi-column-numerical", label = "Columns to calculate RLE for")
 visr.param("addvalue", type = "double", label = "Increment all counts by", default = 0.0, info="The number to add to all counts (necessary if there is 0 in data)")
 visr.param("scale", type = "boolean", label = "Scale by library sizes", default = TRUE, info="Calculate normalization factors and scale the raw library sizes.")
-visr.param("output_RLE", type = "output-multi-column", label = "Output Columns Prefix", info ="RLE values of columns")
+visr.param("output_Normalized", type = "output-multi-column", default="SizeNorm_", label = "Size Normalized Output Columns Prefix", info ="values of columns normalized by the size factor")
+visr.param("output_RLE", type = "output-multi-column", default="RLE_", label = "RLE Output Columns Prefix", info ="RLE values of columns")
 visr.app.end(printjson=TRUE, writefile=T)
 visr.applyParameters()
+#exit()
 
 generateRLE <- function (object) {
   nARR = dim(object)[2]
@@ -31,6 +33,7 @@ if (visr.param.scale) {
   X <- X*f
 }
 
+visr.param.output_Normalized<-X
 visr.param.output_RLE<-generateRLE(X)
 par(mfrow=c(1,1))
 boxplot(x = visr.param.output_RLE, xlab = "Data Columns", ylab = "RLE Value", main = "Relative Log Expression (RLE)", outline = FALSE)
